98ce60402e46a520bf512aae5772133534427ca5
Shohith Kumar K <shohithkumar22@gmail.com>
2026-08-25 11:06:11 +0530

    Initial application setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# GitOps Demo Application
+
+A simple Python Flask application that will be used to demonstrate
+GitOps deployment using Docker, Kubernetes, and ArgoCD.
+
+## Current Version
+
+v1
---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+VERSION = "v1"
+
+@app.route("/")
+def home():
+    return f"GitOps Demo Application - Version {VERSION}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
